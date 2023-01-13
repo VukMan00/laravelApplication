33ed1bcd1a8dfcc48449ecddd8528b0806ca115e
Vuk Manojlovic <vukman619@gmail.com>
2023-01-13 19:21:28 +0100

    Kreiranje modela

---
+++ app/Models/TestQuestion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TestQuestion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'test_id',
+        'question_id',
+    ];
+
+    protected $guarded=[];
+}
